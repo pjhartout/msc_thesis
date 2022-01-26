8595d72ece588fbfdc6c9302fde7bf40fefc7d1e
Philip Hartout <philip.hartout@protonmail.com>
2022-01-26 19:40:07 +0100

    add src repo

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Setup script."""
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    # mandatory
+    name="graph_metrics",
+    # mandatory
+    version="0.1",
+    # mandatory
+    author="Philip Hartout, Tim Kucera",
+    packages=find_packages(),
+    setup_requires=["isort"],
+)
