d7f0e0e2a556da3b1839db88dba8fd80a51736ec
Philip Hartout <philip.hartout@protonmail.com>
2022-01-27 21:30:05 +0100

    add core gnn_metrics package functions to support exploration

---
+++ src/gnn_metrics/utils.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""utils.py
+
+Provides various utilities useful for the project
+"""
+import os
+from typing import List
+
+from tqdm import tqdm
+
+
+def filter_pdb_files(lst_of_files: List[str]) -> List[str]:
+    """Filters pdb files from directory listing
+
+    Args:
+        lst_of_files (List[str]): list containing filenames, possibly including pdb
+        files
+
+    Returns:
+        List: str
+    """
+    return [file for file in lst_of_files if file.endswith("pdb")]
+
+
+def filter_monomers(lst_of_files: List[str]) -> List[str]:
+    """Filters pdb files from directory listing
+
+    Args:
+        lst_of_files (List[str]): list containing filenames, possibly including
+        monomers
+
+    Returns:
+        List: str
+    """
+    monomers = []
+    for file in tqdm(lst_of_files):
+        with open(file) as f:
+            if "MONOMER" in f.read():
+                monomers.append(file)
+    return monomers
+
+
+def make_dir(directory: str) -> None:
+    """Makes directory and does not stop if it is already created.
+
+    Args:
+        directory (str): directory to be created
+    """
+
+    try:
+        os.mkdir(directory)
+    except OSError:
+        print(
+            f"Creation of the directory {directory} failed, probably already "
+            f"exists"
+        )
+    else:
+        print(f"Successfully created the directory {directory}")
