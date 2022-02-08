33094deeb9100f57b8dd40c0695cf3d8bdce19cc
Philip Hartout <philip.hartout@protonmail.com>
2022-02-08 17:37:40 +0100

    add test call for epsilon graph

--- exploring/test_graph_extraction/test_graph_extraction.py
+++ exploring/test_graph_extraction/test_graph_extraction.py
@@ -22,6 +22,7 @@ from proteinggnnmetrics.constants import N_JOBS, REDUCE_DATA
 from proteinggnnmetrics.debug import timeit
 from proteinggnnmetrics.graphs import (
     ContactMap,
+    EpsilonGraph,
     KNNGraph,
     ParallelGraphExtraction,
 )
@@ -37,12 +38,11 @@ def cm_transform(contactmap, file, n_jobs):
 
 @timeit
 def main():
-    random_state = np.random.RandomState(42)
 
     pdb_files = filter_pdb_files(os.listdir(HUMAN_PROTEOME))
     pdb_files = [HUMAN_PROTEOME / file for file in pdb_files]
     if REDUCE_DATA:
-        pdb_files = random.sample(pdb_files, 5000)
+        pdb_files = random.sample(pdb_files, 1000)
 
     parallel_cooords = ParallelCoordinates(n_jobs=N_JOBS)
     coordinates = parallel_cooords.get_coordinates_from_files(
@@ -54,6 +54,20 @@ def main():
     contact_maps = parallelextraction.transform(
         coordinates, contactmap.transform
     )
+
+    knngraph = KNNGraph(n_neighbors=4)
+    knn_graphs = parallelextraction.transform(contact_maps, knngraph.transform)
+
+    epsilongraph = EpsilonGraph(epsilon=6)
+    epsilon_graphs = parallelextraction.transform(
+        contact_maps, epsilongraph.transform
+    )
+
+    plt.imshow(knn_graphs[0].toarray(), interpolation="nearest")
+    plt.savefig("sample_knn.png")
+
+    plt.imshow(epsilon_graphs[0], interpolation="nearest")
+    plt.savefig("sample_epsilon.png")
 
 
 if __name__ == "__main__":
