da269eac552612fd5461d921f98c7af25dd82f0a
Philip Hartout <philip.hartout@protonmail.com>
2022-02-10 17:06:48 +0100

    update documentation

--- src/proteinggnnmetrics/errors.py
+++ src/proteinggnnmetrics/errors.py
@@ -3,3 +3,9 @@ class FileExtentionError(Exception):
     """Raised when file does not contain the right extension."""
 
     pass
+
+
+class GranularityError(Exception):
+    """Raised when granularity is not set correctly"""
+
+    pass
