53dd8a90a5f613fac5d9ea19e57ae1e07e080fd6
Philip Hartout <phartout@ethz.ch>
2022-02-11 14:26:50 +0100

    break up main test function to time and measure footprint

--- exploring/test_graph_extraction/test_graph_extraction.py
+++ exploring/test_graph_extraction/test_graph_extraction.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from Bio.PDB.PDBParser import PDBParser
 from joblib import Parallel, delayed
+from scipy import sparse
 from tqdm import tqdm
 
 from proteinggnnmetrics.constants import N_JOBS, REDUCE_DATA
@@ -28,23 +29,54 @@ from proteinggnnmetrics.utils.utils import filter_pdb_files, tqdm_joblib
 
 @measure_memory
 @timeit
+def get_coords(pdb_files):
+    coord = Coordinates(granularity="CA", n_jobs=N_JOBS)
+    coordinates = coord.transform(pdb_files, granularity="CA")
+    return coordinates
+
+
+@measure_memory
+@timeit
+def get_contactmap(coordinates):
+    contactmap = ContactMap(metric="euclidean")
+    contact_maps = contactmap.transform(coordinates)
+    return contact_maps
+
+
+@measure_memory
+@timeit
+def get_knngraphs(contact_maps):
+    knngraph = KNNGraph(n_neighbors=4)
+    knn_graphs = knngraph.transform(contact_maps)
+    return knn_graphs
+
+
+@measure_memory
+@timeit
+def get_epsilongraph(contact_maps):
+    epsilongraph = EpsilonGraph(epsilon=2)
+    epsilon_graphs = epsilongraph.transform(contact_maps)
+    return epsilon_graphs
+
+
+@measure_memory
+@timeit
 def main():
     pdb_files = filter_pdb_files(os.listdir(HUMAN_PROTEOME))
     pdb_files = [HUMAN_PROTEOME / file for file in pdb_files]
     if REDUCE_DATA:
-        pdb_files = random.sample(pdb_files, 50)
+        pdb_files = random.sample(pdb_files, 10000)
 
-    coord = Coordinates(granularity="CA", n_jobs=N_JOBS)
-    coordinates = coord.transform(pdb_files, granularity="CA")
-
-    contactmap = ContactMap(metric="euclidean")
-    contact_maps = contactmap.transform(coordinates)
-
-    knngraph = KNNGraph(n_neighbors=4)
-    knn_graphs = knngraph.transform(contact_maps)
-
-    epsilongraph = EpsilonGraph(epsilon=6)
-    epsilon_graphs = epsilongraph.transform(contact_maps)
+    coordinates = get_coords(pdb_files)
+    print(f"Coordinates: {len(coordinates)}")
+    contact_maps = get_contactmap(coordinates)
+    coordinates = 0
+    print(f"Contact maps: {len(contact_maps)}")
+    knn_graphs = get_knngraphs(contact_maps)
+    print(f"KNN graphs: {len(knn_graphs)}")
+    knn_graphs = 0
+    epsilon_graphs = get_epsilongraph(contact_maps)
+    print(f"Epsilon graphs: {len(epsilon_graphs)}")
 
 
 if __name__ == "__main__":
