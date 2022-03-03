4a9517776e661d95a2d22ade73e21a5a3baf66a1
Philip Hartout <philip.hartout@protonmail.com>
2022-03-03 18:26:46 +0100

    add work on distance computations

---
+++ exploring/distances/distance_computations.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""distance_computations.py
+
+Computes MMD, other distances from features extracted from protein
+
+"""
+
+import numpy as np
+
+from proteinggnnmetrics.distance import MaximumMeanDiscrepancy
+from proteinggnnmetrics.kernels import LinearKernel, WeisfeilerLehmanKernel
+from proteinggnnmetrics.loaders import (
+    load_descriptor,
+    load_graphs,
+    load_proteins,
+)
+from proteinggnnmetrics.paths import CACHE_DIR
+from proteinggnnmetrics.utils.debug import measure_memory, timeit
+from proteinggnnmetrics.utils.functions import configure, distribute_function
+
+config = configure()
+
+
+def compute_wl_hashes(protein):
+    protein.set_weisfeiler_lehman_hashes(graph_type="knn_graph", n_iter=10)
+    return protein
+
+
+def main():
+
+    # Generate square random matrix
+    # A = np.random.rand(100, 100)
+
+    proteins = load_proteins(CACHE_DIR / "sample_human_proteome_alpha_fold")
+    half = int(len(proteins) / 2)
+
+    # Linear kernel, degree dist
+    degree_histograms = load_descriptor(
+        proteins, descriptor="degree_histogram", graph_type="knn_graph",
+    )
+    linear_kernel = LinearKernel(dense_output=False)
+    mmd = MaximumMeanDiscrepancy(kernel=linear_kernel)
+    result = mmd.evaluate(degree_histograms, degree_histograms)
+
+    # Graph kernel, W-L
+    proteins = distribute_function(
+        compute_wl_hashes,
+        proteins,
+        int(config["COMPUTE"]["N_JOBS"]),
+        "Computing Weisfeiler-Lehman Hashes",
+    )
+    hashes = [
+        protein.descriptors["knn_graph"]["weisfeiler-lehman-hist"]
+        for protein in proteins
+    ]
+    wl_kernel = WeisfeilerLehmanKernel(
+        n_iter=5,
+        normalize=True,
+        n_jobs=int(config["COMPUTE"]["N_JOBS"]),
+        pre_computed_hash=True,
+    )
+    mmd = MaximumMeanDiscrepancy(kernel=wl_kernel)
+    result = mmd.evaluate(hashes[:half], hashes[half:])
+    print(result)
+
+    # TDA distance
+
+
+if __name__ == "__main__":
+    main()
