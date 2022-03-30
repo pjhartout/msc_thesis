d2e3d0ab11ae57656b854dd486680ce96e2effae
Philip Hartout <philip.hartout@protonmail.com>
2022-03-30 21:13:40 +0000

    update coordinates to manage all atoms

--- exploring/biodescriptors/interatomic_clashes.py
+++ exploring/biodescriptors/interatomic_clashes.py
@@ -17,17 +17,15 @@ from proteinggnnmetrics.utils.functions import configure
 config = configure()
 
 N_JOBS = int(config["COMPUTE"]["N_JOBS"])
-REDUCE_DATA = config["DEBUG"]["REDUCE_DATA"]
 VERBOSE = config["RUNTIME"]["VERBOSE"]
 
 
 def main():
     pdb_files = list_pdb_files(HUMAN_PROTEOME)
-    if REDUCE_DATA:
-        pdb_files = pdb_files[:10]
+    pdb_files = pdb_files[:1]
 
     proteins = Coordinates(
-        granularity="CA", n_jobs=N_JOBS, verbose=VERBOSE
+        granularity="all", n_jobs=N_JOBS, verbose=VERBOSE
     ).fit_transform(pdb_files)
     proteins = ContactMap(n_jobs=N_JOBS, verbose=VERBOSE).fit_transform(
         proteins
@@ -35,22 +33,9 @@ def main():
     proteins = InteratomicClash(
         threshold=1.5, n_jobs=N_JOBS, verbose=VERBOSE
     ).fit_transform(proteins)
+
     print(
         f"Ratio of interatomic clashes with low threshold{proteins[0].interatomic_clashes}"
-    )
-
-    proteins = InteratomicClash(
-        threshold=4, n_jobs=N_JOBS, verbose=VERBOSE
-    ).fit_transform(proteins)
-    print(
-        f"Ratio of interatomic clashes with intermediate threshold{proteins[0].interatomic_clashes}"
-    )
-
-    proteins = InteratomicClash(
-        threshold=400, n_jobs=N_JOBS, verbose=VERBOSE
-    ).fit_transform(proteins)
-    print(
-        f"Ratio of interatomic clashes with high threshold{proteins[0].interatomic_clashes}"
     )
 
 
