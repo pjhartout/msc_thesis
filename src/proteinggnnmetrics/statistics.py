e194dbf63f0dbf7700000b896050f530f53f4fc3
Philip Hartout <philip.hartout@protonmail.com>
2022-04-04 08:31:59 +0200

    add all necessary elements of the kernel matrix (not only triu)

--- src/proteinggnnmetrics/statistics.py
+++ src/proteinggnnmetrics/statistics.py
@@ -64,25 +64,72 @@ class MMDTest:
                 range(K_XX.shape[0]), self.m, replace=False
             )
 
-            idx_x = np.array(
-                [
-                    [el1, el2]
-                    for el1, el2 in itertools.product(sample_x, sample_x)
-                ]
+            idx_x = np.concatenate(
+                (
+                    np.array(
+                        [
+                            [el1, el2]
+                            for el1, el2 in itertools.product(
+                                sample_x, sample_x
+                            )
+                        ]
+                    ),
+                    np.array(
+                        [
+                            [el2, el1]
+                            for el1, el2 in itertools.product(
+                                sample_x, sample_x
+                            )
+                            if el1 != el2
+                        ]
+                    ),
+                ),
+                axis=0,
             )
 
-            idx_y = np.array(
-                [
-                    [el1 + K_XX.shape[0], el2 + K_XX.shape[0]]
-                    for el1, el2 in itertools.product(sample_y, sample_y)
-                ]
+            idx_y = np.concatenate(
+                (
+                    np.array(
+                        [
+                            [el1 + K_XX.shape[0], el2 + K_XX.shape[0]]
+                            for el1, el2 in itertools.product(
+                                sample_y, sample_y
+                            )
+                        ]
+                    ),
+                    np.array(
+                        [
+                            [el2 + K_XX.shape[0], el1 + K_XX.shape[0]]
+                            for el1, el2 in itertools.product(
+                                sample_y, sample_y
+                            )
+                            if el1 != el2
+                        ]
+                    ),
+                ),
+                axis=0,
             )
 
-            idx_xy = np.array(
-                [
-                    [el1, el2 + K_XX.shape[0]]
-                    for el1, el2 in itertools.product(sample_x, sample_y)
-                ]
+            idx_xy = np.concatenate(
+                (
+                    np.array(
+                        [
+                            [el1, el2 + K_XX.shape[0]]
+                            for el1, el2 in itertools.product(
+                                sample_x, sample_y
+                            )
+                        ]
+                    ),
+                    np.array(
+                        [
+                            [el2 + K_XX.shape[0], el1]
+                            for el1, el2 in itertools.product(
+                                sample_x, sample_y
+                            )
+                        ]
+                    ),
+                ),
+                axis=0,
             )
 
             sampled_K_XX = full_K[idx_x[:, 0], idx_x[:, 1]]
@@ -103,9 +150,7 @@ class MMDTest:
             # Compute MMD on sampled data.
             sampled_mmds.append(
                 MaximumMeanDiscrepancy(
-                    biased=False,
-                    squared=True,
-                    verbose=False,
+                    biased=False, squared=True, verbose=False,
                 ).compute_from_sums(
                     sampled_K_XX.sum(),
                     sampled_K_YY.sum(),
@@ -114,7 +159,7 @@ class MMDTest:
                     len(sampled_K_YY),
                 )
             )
-
+        print("\n")
         sampled_mmds.append(original_mmd)
         rank = np.where(np.sort(np.array(sampled_mmds)) == original_mmd)[0][0]
         p_value = (self.t + 1 - rank) / (self.t + 1)
@@ -128,17 +173,10 @@ class MMDTest:
         K_XY = self.kernel.compute_gram_matrix(dist_1, dist_2)
 
         mmd_original = MaximumMeanDiscrepancy(
-            biased=False,
-            squared=True,
-            verbose=False,
+            biased=False, squared=True, verbose=False,
         ).compute(K_XX, K_YY, K_XY)
 
         # Compute the p-value
-        p_value = self.rank_statistic(
-            mmd_original,
-            K_XX,
-            K_YY,
-            K_XY,
-        )
+        p_value = self.rank_statistic(mmd_original, K_XX, K_YY, K_XY,)
 
         return p_value
