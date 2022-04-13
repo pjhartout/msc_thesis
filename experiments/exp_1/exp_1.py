d21975bd2d8a9bb535f681b0cea69005e2a94ae3
Philip Hartout <phartout@ethz.ch>
2022-04-13 19:49:53 +0200

    rename seed var

--- experiments/exp_1/exp_1.py
+++ experiments/exp_1/exp_1.py
@@ -57,10 +57,7 @@ def execute_run(cfg, run):
             ),
             (
                 "contact map",
-                ContactMap(
-                    metric="euclidean",
-                    n_jobs=cfg.compute.n_jobs,
-                ),
+                ContactMap(metric="euclidean", n_jobs=cfg.compute.n_jobs,),
             ),
             (
                 "epsilon graph",
@@ -94,7 +91,7 @@ def execute_run(cfg, run):
                         (
                             "gauss",
                             GaussianNoise(
-                                random_state=42,
+                                random_seed=42,
                                 noise_mean=0,
                                 noise_variance=std,
                                 n_jobs=cfg.compute.n_jobs,
