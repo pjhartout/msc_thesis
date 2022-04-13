79a0d8da088983f82f138fdda05b92ad476ae68f
Philip Hartout <philip.hartout@protonmail.com>
2022-04-13 19:52:58 +0200

    add some update slides

---
+++ docs/update_4/example_pipeline.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+base_feature_pipeline = pipeline.Pipeline(
+    [
+        ("coordinates", Coordinates(granularity="CA", n_jobs=12),),
+        ("contact map", ContactMap(metric="euclidean", n_jobs=12,),),
+        ("epsilon graph", EpsilonGraph(epsilon=epsilon, n_jobs=12),),
+    ]
+)
+
+proteins = base_feature_pipeline.fit_transform(paths_to_pdb_files)
+
+mmd = MaximumMeanDiscrepancy(
+    biased=True,
+    squared=True,
+    kernel=WeisfeilerLehmanKernel(
+        n_jobs=12, n_iter=5, normalize=True, biased=True,
+    ),
+).compute(graphs, graphs_perturbed)
---
+++ docs/update_4/example_pipeline_with_noise.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+base_feature_pipeline = pipeline.Pipeline(
+    [
+        ("coordinates", Coordinates(granularity="CA", n_jobs=12),),
+        (
+            "add gaussian noise",
+            GaussianNoise(
+                random_seed=42, noise_mean=0, noise_variance=10, n_jobs=12,
+            ),
+        ),
+        ("contact map", ContactMap(metric="euclidean", n_jobs=12,),),
+        ("epsilon graph", EpsilonGraph(epsilon=epsilon, n_jobs=12),),
+    ]
+)
+
+proteins_perturbed = base_feature_pipeline.fit_transform(paths_to_pdb_files)
+
+graphs = load_graphs(proteins, graph_type="eps_graph")
+graphs_perturbed = load_graphs(proteins_perturbed, graph_type="eps_graph")
+
+mmd = MaximumMeanDiscrepancy(
+    biased=True,
+    squared=True,
+    kernel=WeisfeilerLehmanKernel(
+        n_jobs=12, n_iter=5, normalize=True, biased=True,
+    ),
+).compute(graphs, graphs_perturbed)
