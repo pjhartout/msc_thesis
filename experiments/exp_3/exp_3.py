75c8c7ee4f74e6bba221992d0204b48e20de59fd
Philip Hartout <phartout@ethz.ch>
2022-04-15 14:14:01 +0200

    update experiments for multi-run esm

--- experiments/exp_3/exp_3.py
+++ experiments/exp_3/exp_3.py
@@ -28,10 +28,36 @@ from proteinggnnmetrics.perturbations import Mutation
 from proteinggnnmetrics.utils.functions import flatten_lists
 
 
+def get_longest_protein_dummy_sequence(pdb_files, cfg: DictConfig) -> int:
+    seq = Sequence(n_jobs=cfg.compute.n_jobs)
+    seq_normal = seq.fit_transform(
+        pdb_files[
+            cfg.experiments.proteins.not_perturbed.lower_bound : cfg.experiments.proteins.not_perturbed.upper_bound
+            + 1
+        ]
+    )
+
+    seq_perturbed = seq.fit_transform(
+        pdb_files[
+            cfg.experiments.proteins.perturbed.lower_bound : cfg.experiments.proteins.perturbed.upper_bound
+            + 1
+        ]
+    )
+    longest_sequence = max(
+        max([len(protein.sequence) for protein in seq_normal]),
+        max([len(protein.sequence) for protein in seq_perturbed]),
+    )
+    return longest_sequence
+
+
 @hydra.main(config_path=str(here()) + "/conf/", config_name="conf")
 def main(cfg: DictConfig):
     os.makedirs(here() / cfg.experiments.results, exist_ok=True)
     pdb_files = list_pdb_files(HUMAN_PROTEOME)
+
+    # Get longest proteins of proteins and proteins_perturbed
+    dummy_longest = get_longest_protein_dummy_sequence(pdb_files, cfg)
+
     base_feature_steps = [
         (
             "sequence",
@@ -40,7 +66,10 @@ def main(cfg: DictConfig):
         (
             "esm",
             ESM(
-                size="M", n_jobs=cfg.compute.n_jobs, verbose=cfg.debug.verbose
+                size="M",
+                n_jobs=cfg.compute.n_jobs,
+                verbose=cfg.debug.verbose,
+                longest_sequence=dummy_longest,
             ),
         ),
     ]
@@ -53,58 +82,64 @@ def main(cfg: DictConfig):
             + 1
         ]
     )
-    results = list()
-    for mutation in tqdm(
-        np.arange(
-            cfg.experiments.perturbations.mutation.min,
-            cfg.experiments.perturbations.mutation.max,
-            cfg.experiments.perturbations.mutation.step,
-        ),
-        position=1,
-        leave=False,
-        desc="Mutation probability",
-    ):
-        perturb_feature_steps = flatten_lists(
-            [
-                base_feature_steps[:1]
-                + [
-                    (
-                        "mutate",
-                        Mutation(
-                            p_mutate=mutation,
-                            random_seed=42,
-                            n_jobs=cfg.compute.n_jobs,
-                            verbose=cfg.debug.verbose,
-                        ),
-                    )
+
+    for run in range(cfg.experiments.n_runs):
+        results = list()
+        for mutation in tqdm(
+            np.arange(
+                cfg.experiments.perturbations.mutation.min,
+                cfg.experiments.perturbations.mutation.max,
+                cfg.experiments.perturbations.mutation.step,
+            ),
+            position=1,
+            leave=False,
+            desc="Mutation probability",
+        ):
+            perturb_feature_steps = flatten_lists(
+                [
+                    base_feature_steps[:1]
+                    + [
+                        (
+                            "mutate",
+                            Mutation(
+                                p_mutate=mutation,
+                                random_state=np.random.RandomState(42),
+                                n_jobs=cfg.compute.n_jobs,
+                                verbose=cfg.debug.verbose,
+                            ),
+                        )
+                    ]
+                    + base_feature_steps[1:]
                 ]
-                + base_feature_steps[1:]
-            ]
-        )
-        perturb_feature_pipeline = pipeline.Pipeline(
-            perturb_feature_steps, verbose=cfg.debug.verbose
-        )
-        proteins_perturbed = perturb_feature_pipeline.fit_transform(
-            pdb_files[
-                cfg.experiments.proteins.perturbed.lower_bound : cfg.experiments.proteins.perturbed.upper_bound
-                + 1
-            ]
-        )
-        embeddings = [protein.embeddings["esm"] for protein in proteins]
-        embeddings_perturbed = [
-            protein.embeddings["esm"] for protein in proteins_perturbed
-        ]
+            )
+            perturb_feature_pipeline = pipeline.Pipeline(
+                perturb_feature_steps, verbose=cfg.debug.verbose
+            )
+            proteins_perturbed = perturb_feature_pipeline.fit_transform(
+                pdb_files[
+                    cfg.experiments.proteins.perturbed.lower_bound : cfg.experiments.proteins.perturbed.upper_bound
+                    + 1
+                ]
+            )
+            embeddings = np.array(
+                [protein.embeddings["esm"] for protein in proteins]
+            )
+            embeddings_perturbed = np.array(
+                [protein.embeddings["esm"] for protein in proteins_perturbed]
+            )
 
-        mmd = MaximumMeanDiscrepancy(
-            biased=True,
-            squared=True,
-            kernel=LinearKernel(n_jobs=cfg.compute.n_jobs),  # type: ignore
-        ).compute(embeddings, embeddings_perturbed)
-        results.append({"mmd": mmd, "twist": mutation})
+            mmd = MaximumMeanDiscrepancy(
+                biased=True,
+                squared=True,
+                kernel=LinearKernel(n_jobs=cfg.compute.n_jobs),  # type: ignore
+            ).compute(embeddings, embeddings_perturbed)
+            results.append({"mmd": mmd, "twist": mutation})
 
-    results = pd.DataFrame(results).to_csv(
-        here() / cfg.experiments.results / "mmd_single_run_twist.csv"
-    )
+        results = pd.DataFrame(results).to_csv(
+            here()
+            / cfg.experiments.results
+            / f"mmd_single_run_mutation_run_{run}.csv"
+        )
 
 
 if __name__ == "__main__":
