d979c1f5ea199b518214e0d0c48d575357540e9e
Philip Hartout <philip.hartout@protonmail.com>
2022-05-18 07:44:20 +0200

    add script to build data used in systematic assessment

---
+++ experiments/data_index_builder.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""experiment_combination_builder.py
+
+This script generates all the combination of experiments to compute
+
+"""
+
+import os
+import random
+
+import hydra
+import pandas as pd
+from omegaconf import DictConfig
+from pyprojroot import here
+
+from proteinggnnmetrics.loaders import list_pdb_files
+from proteinggnnmetrics.paths import HUMAN_PROTEOME
+from proteinggnnmetrics.utils.functions import make_dir
+
+
+@hydra.main(config_path=str(here()) + "/conf/", config_name="conf")
+def main(cfg):
+    pdb_files = list_pdb_files(HUMAN_PROTEOME)
+    make_dir(here() / cfg.meta.data.data_split_dir)
+    for run in range(cfg.meta.data.n_runs):
+        sampled_files = random.Random(run).sample(
+            pdb_files, cfg.meta.data.sample_size * 2
+        )
+        half = cfg.meta.data.sample_size
+        df_unperturbed = pd.DataFrame(sampled_files[:half], columns=["pdb_id"])
+        df_perturbed = pd.DataFrame(sampled_files[half:], columns=["pdb_id"])
+
+        df_unperturbed.to_csv(
+            os.path.join(
+                here() / cfg.meta.data.data_split_dir,
+                f"data_split_{run}_unperturbed.csv",
+            ),
+            index=False,
+        )
+        df_perturbed.to_csv(
+            os.path.join(
+                here() / cfg.meta.data.data_split_dir,
+                f"data_split_{run}_perturbed.csv",
+            ),
+            index=False,
+        )
+
+    print("Data for different runs is set")
+
+
+if __name__ == "__main__":
+    main()
