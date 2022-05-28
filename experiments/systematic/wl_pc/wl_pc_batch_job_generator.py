8c3e16502bca4d06ace4a7597a45ded8607c5aa5
Philip Hartout <philip.hartout@protonmail.com>
2022-05-28 19:38:04 +0200

    update jobs

--- experiments/systematic/wl_pc/wl_pc_batch_job_generator.py
+++ experiments/systematic/wl_pc/wl_pc_batch_job_generator.py
@@ -27,25 +27,24 @@ fi\n
 )
 def main(cfg):
     wl_pc = open(
-        here() / "experiments/systematic" / "wl_pc_job_array.sh",
+        here() / "experiments/systematic/wl_pc" / "wl_pc_job_array.sh",
         "w",
     )  # write mode
     wl_pc.write(f"#!/bin/bash \n")
     wl_pc.write(f"\n\n")
     wl_pc.write(f"cd /home/phartout/Documents/Git/msc_thesis/ \n")
     wl_pc.write(f"export PATH=/home/phartout/.anaconda3/bin:$PATH\n\n")
-    slurm_string = "srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py"
+    slurm_string = "srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py"
 
-    for n_iters in cfg.meta.kernels[3]["weisfeiler-lehman"][0]["n_iter"]:
-        for eps in cfg.meta.representations[0]["eps_graph"]:
-            job_param = f"{slurm_string} +n_iter={n_iters} +graph_type=eps_graph +graph_extraction_parameter={eps} \n"
-            wl_pc.write(job_param)
-            wl_pc.write(build_fail_string(job_param))
+    for eps in cfg.meta.representations[0]["eps_graph"]:
+        job_param = f"{slurm_string} +graph_type=eps_graph +graph_extraction_parameter={eps} \n"
+        wl_pc.write(job_param)
+        wl_pc.write(build_fail_string(job_param))
 
-        for k in cfg.meta.representations[1]["knn_graph"]:
-            job_param = f"{slurm_string} +n_iter={n_iters} +graph_type=knn_graph +graph_extraction_parameter={k} \n"
-            wl_pc.write(job_param)
-            wl_pc.write(build_fail_string(job_param))
+    for k in cfg.meta.representations[1]["knn_graph"]:
+        job_param = f"{slurm_string} +graph_type=knn_graph +graph_extraction_parameter={k} \n"
+        wl_pc.write(job_param)
+        wl_pc.write(build_fail_string(job_param))
 
     wl_pc.write(f'echo "Done"\n')
     wl_pc.close()
--- experiments/systematic/wl_pc/wl_pc_job_array.sh
+++ experiments/systematic/wl_pc/wl_pc_job_array.sh
@@ -4,273 +4,57 @@
 cd /home/phartout/Documents/Git/msc_thesis/ 
 export PATH=/home/phartout/.anaconda3/bin:$PATH
 
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=eps_graph +graph_extraction_parameter=8 
+srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=eps_graph +graph_extraction_parameter=8 
 
 if [ $? -eq 0 ]; then
    echo OK
 else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=eps_graph +graph_extraction_parameter=8 
+   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=eps_graph +graph_extraction_parameter=8 
 " >> failed_jobs.txt
 fi
 
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=eps_graph +graph_extraction_parameter=16 
+srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=eps_graph +graph_extraction_parameter=16 
 
 if [ $? -eq 0 ]; then
    echo OK
 else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=eps_graph +graph_extraction_parameter=16 
+   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=eps_graph +graph_extraction_parameter=16 
 " >> failed_jobs.txt
 fi
 
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=eps_graph +graph_extraction_parameter=32 
+srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=eps_graph +graph_extraction_parameter=32 
 
 if [ $? -eq 0 ]; then
    echo OK
 else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=eps_graph +graph_extraction_parameter=32 
+   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=eps_graph +graph_extraction_parameter=32 
 " >> failed_jobs.txt
 fi
 
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=knn_graph +graph_extraction_parameter=2 
+srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=knn_graph +graph_extraction_parameter=2 
 
 if [ $? -eq 0 ]; then
    echo OK
 else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=knn_graph +graph_extraction_parameter=2 
+   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=knn_graph +graph_extraction_parameter=2 
 " >> failed_jobs.txt
 fi
 
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=knn_graph +graph_extraction_parameter=6 
+srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=knn_graph +graph_extraction_parameter=6 
 
 if [ $? -eq 0 ]; then
    echo OK
 else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=knn_graph +graph_extraction_parameter=6 
+   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=knn_graph +graph_extraction_parameter=6 
 " >> failed_jobs.txt
 fi
 
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=knn_graph +graph_extraction_parameter=8 
+srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=knn_graph +graph_extraction_parameter=8 
 
 if [ $? -eq 0 ]; then
    echo OK
 else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=1 +graph_type=knn_graph +graph_extraction_parameter=8 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=eps_graph +graph_extraction_parameter=8 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=eps_graph +graph_extraction_parameter=8 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=eps_graph +graph_extraction_parameter=16 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=eps_graph +graph_extraction_parameter=16 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=eps_graph +graph_extraction_parameter=32 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=eps_graph +graph_extraction_parameter=32 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=knn_graph +graph_extraction_parameter=2 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=knn_graph +graph_extraction_parameter=2 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=knn_graph +graph_extraction_parameter=6 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=knn_graph +graph_extraction_parameter=6 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=knn_graph +graph_extraction_parameter=8 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=2 +graph_type=knn_graph +graph_extraction_parameter=8 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=eps_graph +graph_extraction_parameter=8 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=eps_graph +graph_extraction_parameter=8 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=eps_graph +graph_extraction_parameter=16 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=eps_graph +graph_extraction_parameter=16 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=eps_graph +graph_extraction_parameter=32 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=eps_graph +graph_extraction_parameter=32 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=knn_graph +graph_extraction_parameter=2 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=knn_graph +graph_extraction_parameter=2 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=knn_graph +graph_extraction_parameter=6 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=knn_graph +graph_extraction_parameter=6 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=knn_graph +graph_extraction_parameter=8 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=3 +graph_type=knn_graph +graph_extraction_parameter=8 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=eps_graph +graph_extraction_parameter=8 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=eps_graph +graph_extraction_parameter=8 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=eps_graph +graph_extraction_parameter=16 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=eps_graph +graph_extraction_parameter=16 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=eps_graph +graph_extraction_parameter=32 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=eps_graph +graph_extraction_parameter=32 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=knn_graph +graph_extraction_parameter=2 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=knn_graph +graph_extraction_parameter=2 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=knn_graph +graph_extraction_parameter=6 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=knn_graph +graph_extraction_parameter=6 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=knn_graph +graph_extraction_parameter=8 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=5 +graph_type=knn_graph +graph_extraction_parameter=8 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=eps_graph +graph_extraction_parameter=8 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=eps_graph +graph_extraction_parameter=8 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=eps_graph +graph_extraction_parameter=16 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=eps_graph +graph_extraction_parameter=16 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=eps_graph +graph_extraction_parameter=32 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=eps_graph +graph_extraction_parameter=32 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=knn_graph +graph_extraction_parameter=2 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=knn_graph +graph_extraction_parameter=2 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=knn_graph +graph_extraction_parameter=6 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=knn_graph +graph_extraction_parameter=6 
-" >> failed_jobs.txt
-fi
-
-srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=knn_graph +graph_extraction_parameter=8 
-
-if [ $? -eq 0 ]; then
-   echo OK
-else
-   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc.py +n_iter=10 +graph_type=knn_graph +graph_extraction_parameter=8 
+   echo "FAILED srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/wl_pc/wl_pc.py +graph_type=knn_graph +graph_extraction_parameter=8 
 " >> failed_jobs.txt
 fi
 
