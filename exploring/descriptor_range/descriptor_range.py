316dd0b8682518c5b4ea480a33ec4aa67bb60067
Philip Hartout <philip.hartout@protonmail.com>
2022-05-30 15:55:56 +0200

    adding max of distance histogram

--- exploring/descriptor_range/descriptor_range.py
+++ exploring/descriptor_range/descriptor_range.py
@@ -7,8 +7,9 @@ What is:
     - The range of the laplacian
     - The range of the degree?
     
-max degree  719
+max degree 719
 laplacian  1.2586714829332633
+distance   462.5783386230469
 """
 import logging
 import os
@@ -22,13 +23,17 @@ import pandas as pd
 import seaborn as sns
 from gtda import pipeline
 
-from proteinmetrics.descriptors import DegreeHistogram, LaplacianSpectrum
+from proteinmetrics.descriptors import (
+    DegreeHistogram,
+    DistanceHistogram,
+    LaplacianSpectrum,
+)
 from proteinmetrics.graphs import ContactMap, EpsilonGraph
 from proteinmetrics.loaders import list_pdb_files
 from proteinmetrics.paths import HUMAN_PROTEOME
 from proteinmetrics.pdb import Coordinates
 
-N_JOBS = 40
+N_JOBS = 100
 
 log = logging.getLogger(__name__)
 
@@ -48,26 +53,6 @@ def main():
             "eps_graph",
             EpsilonGraph(epsilon=32, n_jobs=N_JOBS, verbose=True),
         ),
-        (
-            "laplacian",
-            LaplacianSpectrum(
-                graph_type="eps_graph",
-                n_bins=100,
-                bin_range=(0, 100),
-                n_jobs=N_JOBS,
-                verbose=True,
-            ),
-        ),
-        (
-            "degree",
-            DegreeHistogram(
-                graph_type="eps_graph",
-                n_bins=100,
-                bin_range=(0, 100),
-                n_jobs=N_JOBS,
-                verbose=True,
-            ),
-        ),
     ]
 
     proteins = pipeline.Pipeline(base_feature_steps).fit_transform(pdb_files)
@@ -83,11 +68,18 @@ def main():
             for protein in proteins
         ]
     )
+    max_distance = max(
+        [np.max(protein.contact_map.flatten()) for protein in proteins]
+    )
+
     print(f"max degree  {max_degree}")
     log.info(f"max degree  {max_degree}")
 
     print(f"laplacian  {max_laplacian}")
     log.info(f"laplacian  {max_laplacian}")
+
+    print(f"max distance {max_distance}")
+    log.info(f"max distance {max_distance}")
 
 
 if __name__ == "__main__":
