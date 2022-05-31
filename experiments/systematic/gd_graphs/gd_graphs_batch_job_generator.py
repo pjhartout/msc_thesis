398ea2e94005867027e4bb5c50764d35d44555a6
Philip Hartout <philip.hartout@protonmail.com>
2022-05-31 19:51:40 +0200

    update mem and cpu reqs

--- experiments/systematic/gd_graphs/gd_graphs_batch_job_generator.py
+++ experiments/systematic/gd_graphs/gd_graphs_batch_job_generator.py
@@ -34,7 +34,7 @@ def main(cfg):
     wl_pc.write(f"\n\n")
     wl_pc.write(f"cd /home/phartout/Documents/Git/msc_thesis/ \n")
     wl_pc.write(f"export PATH=/home/phartout/.anaconda3/bin:$PATH\n\n")
-    slurm_string = "srun --cpus-per-task 140 --mem-per-cpu 7G --exclude=bs-hpsvr08 poetry run python experiments/systematic/gd_graphs/gd_graphs.py"
+    slurm_string = "srun --cpus-per-task 50 --mem-per-cpu 9.9G --exclude=bs-hpsvr08 poetry run python experiments/systematic/gd_graphs/gd_graphs.py"
 
     descriptors = [
         "degree_histogram",
